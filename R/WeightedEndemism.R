e8e8efc74683c5720d42d9ca30f8dc2256d8193e
Klaus Schliep <klaus.schliep@gmail.com>
2019-12-02 04:06:09 -0500

    bug fix

--- R/WeightedEndemism.R
+++ R/WeightedEndemism.R
@@ -22,28 +22,28 @@
 #' @return A data frame of species traits by site.
 #'
 #' @references
-#' Crisp, M.D., Laffan, S., Linder, H.P. & Monro, A. (2001) Endemism in the Australian flora. 
+#' Crisp, M.D., Laffan, S., Linder, H.P. & Monro, A. (2001) Endemism in the Australian flora.
 #' \emph{Journal of Biogeography} \strong{28}: 183–198.
-#' 
-#' Laffan, S.W., & Crisp, M.D. (2003) Assessing endemism at multiple spatial scales, 
-#' with an example from the Australian vascular flora. \emph{Journal of Biogeography} \strong{30}: 511–520. 
+#'
+#' Laffan, S.W., & Crisp, M.D. (2003) Assessing endemism at multiple spatial scales,
+#' with an example from the Australian vascular flora. \emph{Journal of Biogeography} \strong{30}: 511–520.
 #' @examples
 #' require(raster)
 #' data(africa)
-#' 
+#'
 #' Endm <- weighted.endemism(africa$comm)
 #' m <- merge(africa$polys, Endm, by="grids")
 #' m <- m[!is.na(m@data$WE),]
-#' 
-#' plot.swatch(m, values = m$WE, k=20) 
-#' 
+#'
+#' plot.swatch(m, values = m$WE, k=20)
+#'
 #' @export weighted.endemism
 weighted.endemism <- function(x){
   tmp <- .SpecRich(x)
   index <- match(x$grids, tmp$grids)
   SR <- tmp$SR[index]
   ff <- table(x$species)
-  x$WE <- SR/ff[x$species]
+  x$WE <- as.numeric(SR/ff[x$species]) # added as.number
   tmp <- as.data.table(x)
   res <- tmp[, sum(WE), by=grids]
   res <- res[, list(grids, WE=V1)]
