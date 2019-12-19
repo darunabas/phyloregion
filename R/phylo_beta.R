128c3f0bdc0b4f85f6bee8aa31b30112488dae4b
Klaus Schliep <klaus.schliep@gmail.com>
2019-12-19 15:13:36 +0100

    nicer formatting

--- R/phylo_beta.R
+++ R/phylo_beta.R
@@ -1,59 +1,47 @@
-#
-# occurence --> phylogenetic occurence --> phylo beta
-#           --> beta
-#  beta  == beta core (a,b,c)
-#  Matrix package, sparse binary data
-#
-#  betapart indices Jaccard, Simpson, etc.
-#
+phylo_com <- function(tip, phy) {
+  if (!inherits(phy, "phylo"))
+    stop("object \"phy\" is not of class \"phylo\"")
+  done_v <- logical(length(phy$tip.label) + phy$Nnode)
+  rootnd <- getRoot(phy)
+  if (is.character(tip)) tip <- match(tip, c(phy$tip.label, phy$node.label))
+  tip <- as.integer(tip)
+  res <- pvec <- integer(max(phy$edge))
+  pvec[phy$edge[, 2]] <- phy$edge[, 1]
 
-phylo_com <- function(tip, phy){
-    if (!inherits(phy, "phylo"))
-        stop("object \"phy\" is not of class \"phylo\"")
-    Ntips <- length(phy$tip.label)
-    Nnodes <- phy$Nnode
-    done_v <- logical(Ntips + Nnodes)
-    rootnd <- Ntips +1L #getRoot(phy)
-    if (is.character(tip)) tip <- match(tip, c(phy$tip.label, phy$node.label))
-    tip <- as.integer(tip)
-    res <- pvec <- integer(max(phy$edge))
-    pvec[phy$edge[, 2]] <- phy$edge[, 1]
-
-    res[seq_along(tip)] <- tip
-    l <- length(tip) + 1L
-    res[l] <- rootnd
-    done_v[rootnd] <- TRUE
-    for(k in tip) {
-        nd <- pvec[k]
-        done <- done_v[nd]
-        while(!done){
-            done_v[nd] <- TRUE
-            l <- l+1
-            res[l] <- nd
-            nd <- pvec[nd]
-            done <- done_v[nd]
-        }
+  res[seq_along(tip)] <- tip
+  l <- length(tip) + 1L
+  res[l] <- rootnd
+  done_v[rootnd] <- TRUE
+  for (k in tip) {
+    nd <- pvec[k]
+    done <- done_v[nd]
+    while (!done) {
+      done_v[nd] <- TRUE
+      l <- l + 1
+      res[l] <- nd
+      nd <- pvec[nd]
+      done <- done_v[nd]
     }
-    sort(res[1:l])
+  }
+  sort(res[1:l])
 }
 
 
-phylo_community <- function(x, phy){
+phylo_community <- function(x, phy) {
   el <- numeric(max(phy$edge))
-  el[phy$edge[,2]] <- phy$edge.length
+  el[phy$edge[, 2]] <- phy$edge.length
   x <- x[, phy$tip.label]
-  if(is.matrix(x) | is(x, "sparseMatrix")){
+  if (is.matrix(x) | is(x, "sparseMatrix")) {
     x <- as.splits(x)
   }
-  #  x <- phangorn::changeOrder(x, phy$tip.label)
-  if(is.character(x) | is.numeric(x))  y <- list(phylo_com(x, phy))
-  if(is.list(x)){
-    y <- lapply(x, function(x, phy)phylo_com(x, phy), phy)
+  if (is.character(x) | is.numeric(x)) y <- list(phylo_com(x, phy))
+  if (is.list(x)) {
+    y <- lapply(x, function(x, phy) phylo_com(x, phy), phy)
   }
-  if(is.null(y)) return(NULL)
+  if (is.null(y)) return(NULL)
   attr(y, "edge.length") <- el
   attr(y, "labels") <- c(phy$tip.label, as.character(Ntip(phy)
-                                                     + (1:Nnode(phy))))
+  + (1:Nnode(phy))))
   class(y) <- c("phylo_community", "splits")
   y
 }
@@ -75,58 +63,58 @@ phylo_community <- function(x, phy){
 #' @seealso \code{\link{read.community}}, \code{\link{phylo.betapart.core}}
 #' @examples
 #' library(ape)
-#' tree <- read.tree(text ="((t1:1,t2:1)N2:1,(t3:1,t4:1)N3:1)N1;")
-#' com <- matrix(c(1,0,1,1,0,0,
-#'                 1,0,0,1,1,0,
-#'                 1,1,1,1,1,1,
-#'                 0,0,1,1,0,1), 6, 4,
-#'               dimnames=list(paste0("g",1:6), tree$tip.label))
+#' tree <- read.tree(text = "((t1:1,t2:1)N2:1,(t3:1,t4:1)N3:1)N1;")
+#' com <- matrix(c(1, 0, 1, 1, 0, 0,
+#'   1, 0, 0, 1, 1, 0,
+#'   1, 1, 1, 1, 1, 1,
+#'   0, 0, 1, 1, 0, 1), 6, 4,
+#' dimnames = list(paste0("g", 1:6), tree$tip.label))
 #' pbc <- phylobeta_core(com, tree)
 #' pb <- phylobeta(com, tree)
-#'
 #' @rdname phylobeta
 #' @importFrom fastmatch fmatch
 #' @importFrom betapart phylo.beta.multi phylo.beta.pair
+#' @importFrom phangorn getRoot
 #' @export
-phylobeta_core <- function(x, phy){
-  if(!identical(sort(colnames(x)), sort(phy$tip.label)))
+phylobeta_core <- function(x, phy) {
+  if (!identical(sort(colnames(x)), sort(phy$tip.label)))
     stop("Labels of community matrix and tree differ!")
   x <- phylo_community(x, phy)
   l <- length(x)
   el <- attr(x, "edge.length")
-  pd_tmp <- vapply(x, function(x, el)sum(el[x]), 0, el)
+  pd_tmp <- vapply(x, function(x, el) sum(el[x]), 0, el)
 
   Labels <- names(x)
   class(x) <- NULL
-  SHARED <- vector("numeric", l*(l-1)/2)
-  B <- vector("numeric", l*(l-1)/2)
-  C <- vector("numeric", l*(l-1)/2)
+  SHARED <- vector("numeric", l * (l - 1) / 2)
+  B <- vector("numeric", l * (l - 1) / 2)
+  C <- vector("numeric", l * (l - 1) / 2)
 
   k <- 1
-  for(i in 1:(l-1)){
+  for (i in 1:(l - 1)) {
     xi <- x[[i]]
-    for(j in (i+1):l){
+    for (j in (i + 1):l) {
       #   sum(el[fast_intersect(x[[i]], x[[j]])])
-      SHARED[k] <- sum(el[xi[fmatch(x[[j]], xi, 0L)] ])
+      SHARED[k] <- sum(el[xi[fmatch(x[[j]], xi, 0L)]])
       B[k] <- pd_tmp[i] - SHARED[k]
       C[k] <- pd_tmp[j] - SHARED[k]
-      k <- k+1
+      k <- k + 1
     }
   }
 
-  sum.not.shared <- B+C
-  max.not.shared <- pmax(B,C)
-  min.not.shared <- pmin(B,C)
+  sum.not.shared <- B + C
+  max.not.shared <- pmax(B, C)
+  min.not.shared <- pmin(B, C)
 
-  at <- structure(list(Labels=Labels, Size = l, class = "dist", Diag = FALSE,
-        Upper = FALSE), .Names = c("Labels", "Size", "class", "Diag", "Upper"))
+  at <- structure(list(Labels = Labels, Size = l, class = "dist", Diag = FALSE,
+    Upper = FALSE), .Names = c("Labels", "Size", "class", "Diag", "Upper"))
   attributes(SHARED) <- at
   attributes(sum.not.shared) <- at
   attributes(max.not.shared) <- at
   attributes(min.not.shared) <- at
-  res <- list(sumSi=sum(pd_tmp), St=sum(el), shared=SHARED,
-              sum.not.shared = sum.not.shared,
-              max.not.shared=max.not.shared, min.not.shared=min.not.shared)
+  res <- list(sumSi = sum(pd_tmp), St = sum(el), shared = SHARED,
+    sum.not.shared = sum.not.shared,
+    max.not.shared = max.not.shared, min.not.shared = min.not.shared)
   class(res) <- "phylo.betapart"
   res
 }
@@ -134,7 +122,7 @@ phylobeta_core <- function(x, phy){
 
 #' @rdname phylobeta
 #' @export
-phylobeta <- function(x, phy, index.family="sorensen"){
+phylobeta <- function(x, phy, index.family = "sorensen") {
   res <- phylobeta_core(x, phy)
   p <- phylo.beta.pair(res, index.family = index.family)
   if (index.family == "simpson") {
@@ -172,9 +160,8 @@ phylobeta <- function(x, phy, index.family="sorensen"){
 #' submat <- match_phylo_comm(tree, x)$com
 #' @importFrom ape keep.tip
 #' @export
-match_phylo_comm <- function (phy, comm)
-{
-  if (!(is.data.frame(comm) | is.matrix(comm) | inherits(comm, "Matrix") )) {
+match_phylo_comm <- function(phy, comm) {
+  if (!(is.data.frame(comm) | is.matrix(comm) | inherits(comm, "Matrix"))) {
     stop("Community data should be a data.frame or matrix with samples in rows
          and taxa in columns")
   }
@@ -201,9 +188,9 @@ match_phylo_comm <- function (phy, comm)
 #' @seealso \code{\link{betapart.core}}
 #' @export
 ## non phylogenetic version
-beta_core <- function (x) {
+beta_core <- function(x) {
   if (!inherits(x, "Matrix")) x <- Matrix(x)
-  shared <- as.matrix( tcrossprod(x) )# %*% t(x)
+  shared <- as.matrix(tcrossprod(x)) # %*% t(x)
   not.shared <- abs(sweep(shared, 2, diag(shared)))
   sumSi <- sum(diag(shared))
   St <- sum(colSums(x) > 0)
@@ -212,16 +199,16 @@ beta_core <- function (x) {
   max.not.shared <- pmax(not.shared, t(not.shared))
   min.not.shared <- pmin(not.shared, t(not.shared))
   computations <- list(data = x, sumSi = sumSi, St = St, a = a, shared = shared,
-                       not.shared = not.shared, sum.not.shared = sum.not.shared,
-                       max.not.shared = max.not.shared,
-                       min.not.shared = min.not.shared)
+    not.shared = not.shared, sum.not.shared = sum.not.shared,
+    max.not.shared = max.not.shared,
+    min.not.shared = min.not.shared)
   class(computations) <- "betapart"
   computations
 }
 
 #' @rdname beta_diss
 #' @export
-beta_diss <- function(x, index.family="sorensen"){ #, index="Simpson"
+beta_diss <- function(x, index.family = "sorensen") {
   computations <- beta_core(x)
   p <- beta.pair(computations, index.family = index.family)
   if (index.family == "simpson") {
--- R/phylo_builder.R
+++ R/phylo_builder.R
@@ -7,7 +7,8 @@
 #'
 #' @param species A vector or matrix containing a species list
 #' @param tree a phylogenetic tree (object of class phylo)
-#' @seealso \code{\link[phangorn]{add.tips}}
+#' @seealso \code{\link[phangorn]{add.tips}}, \code{\link[ape]{label2table}},
+#' \code{\link[ape]{stripLabel}}
 #' @keywords bioregion
 #' @importFrom ape drop.tip stripLabel label2table getMRCA
 #' @importFrom parallel mclapply
@@ -16,66 +17,64 @@
 #' require(ape)
 #' data("africa")
 #' x <- africa$comm
-#' phylobuilder(species=x$species, tree=africa$phylo)
+#' phylobuilder(species = x$species, tree = africa$phylo)
 #' @export
-phylobuilder <- function (species, tree)
-{
-  taxonomy <- c("species", "genus", "family")
-#  ,"suborder", "order", "class", "phylum", "kingdom")
-# branching phylomatic_names
-  if(is.matrix(species) | is.data.frame(species)) species_list <- species
-
-  if(is.factor(species)) species <- as.character(species)
-  if(is.vector(species)) {
-    species <- stripLabel(species)
+phylobuilder <- function(species, tree) {  # , sep=NULL,
+  taxonomy <- c("subspecies", "species", "genus", "family")
+  #  ,"suborder", "order", "class", "phylum", "kingdom",  ) "subspecies"
+  # branching phylomatic_names subspecies
+  if (is.matrix(species) | is.data.frame(species)) species_list <- species
+  if (is.factor(species)) species <- as.character(species)
+  if (is.vector(species)) {
+    species <- unique(species)
+    species <- stripLabel(species, subsp = FALSE)
     species_list <- label2table(species)
     species_list$species <- species
   }
 
-  if(is.matrix(species_list) | is.data.frame(species_list)){
+  if (is.matrix(species_list) | is.data.frame(species_list)) {
     colnames(species_list) <- tolower(colnames(species_list))
     nam <- intersect(taxonomy, colnames(species_list))
     species_list <- species_list[, nam]
   }
 
-  species_list[sapply(species_list, is.factor)] <-
-            lapply(species_list[sapply(species_list, is.factor)], as.character)
+  species_list[vapply(species_list, is.factor, FALSE)] <-
+    lapply(species_list[vapply(species_list, is.factor, FALSE)], as.character)
 
-  if (any(duplicated(species_list$species))){
+  if (any(duplicated(species_list$species))) {
     warning("Duplicated species detected and removed.")
     species_list <- species_list[!duplicated(species_list$species), ]
   }
-  Ori <- species_list
+
   species_list$species <- gsub(" ", "_", species_list$species)
   tree$tip.label <- gsub(" ", "_", tree$tip.label)
-# need to prune away subspecies
-#  tree$tip.label <- stripLabel(tree$tip.label)
+  #  tree$tip.label <- stripLabel(tree$tip.label)
 
   species_list$species <- gsub("(^[[:alpha:]])", "\\U\\1", species_list$species,
-                         perl = TRUE)
+    perl = TRUE)
   species_list$genus <- gsub("(^[[:alpha:]])", "\\U\\1", species_list$genus,
-                       perl = TRUE)
-  if(!is.null(species_list$family )) species_list$family <-
-       gsub("(^[[:alpha:]])", "\\U\\1", species_list$family, perl = TRUE)
+    perl = TRUE)
+  if (!is.null(species_list$family)) species_list$family <-
+    gsub("(^[[:alpha:]])", "\\U\\1", species_list$family, perl = TRUE)
 
   T1 <- tree
-  add_tip <- which(is.na(match(species_list$species, tree$tip.label)) )
+  add_tip <- which(is.na(match(species_list$species, tree$tip.label)))
 
-  species_list$add <- rep("tree", nrow(species_list) )
+  species_list$add <- rep("tree", nrow(species_list))
   species_list$add[add_tip] <- "missing"
 
-  if (length(add_tip) > 0){
+  if (length(add_tip) > 0) {
 
     unique_genera <- unique(species_list$genus[add_tip])
     tree_genera <- as.character(label2table(T1$tip)$"genus")
 
-    fun <- function(g, g_tree){
+    fun <- function(g, g_tree) {
       tmp <- match(g_tree, g)
       l <- length(g)
       res <- vector("list", l)
-      for(i in seq_along(tmp)){
+      for (i in seq_along(tmp)) {
         a <- tmp[i]
-        if(!is.na(a))res[[a]] <- c(res[[a]], i)
+        if (!is.na(a)) res[[a]] <- c(res[[a]], i)
       }
       res
     }
@@ -87,33 +86,32 @@ phylobuilder <- function (species, tree)
     nn <- nn_unique[tmp]
     ll <- ll_unique[tmp]
 
-    tips <- tree$tip.label
     tips2add <- NULL
     where2add <- NULL
-    if(any(ll==1)){
-      tmp_num <- unlist(nn[ll==1])
-      num <- T1$edge[match(tmp_num, T1$edge[,2]), 1]
+    if (any(ll == 1)) {
+      tmp_num <- unlist(nn[ll == 1])
+      num <- T1$edge[match(tmp_num, T1$edge[, 2]), 1]
       where2add <- c(where2add, num)
-      tips2add <- species_list$species[add_tip[ll==1]]
+      tips2add <- species_list$species[add_tip[ll == 1]]
     }
-    if(any(ll > 1)){
-      tips2add <- c(tips2add, species_list$species[add_tip[ll > 1]] )
-      fun2 <- function(x, tree) ifelse(length(x)>1, getMRCA(tree, x), 0)
-      num <- mclapply(nn_unique,  fun2 , tree=T1)
+    if (any(ll > 1)) {
+      tips2add <- c(tips2add, species_list$species[add_tip[ll > 1]])
+      fun2 <- function(x, tree) ifelse(length(x) > 1, getMRCA(tree, x), 0)
+      num <- mclapply(nn_unique,  fun2, tree = T1)
       num <- (unlist(num)[tmp])[ll > 1]
       where2add <- c(where2add, unlist(num))
     }
-    if(!is.null(where2add)) T1 <- add.tips(T1, tips2add, where = where2add)
-    species_list$add[add_tip[ll>0]] <- "genus"
-    add_tip <- add_tip[ll==0]
+    if (!is.null(where2add)) T1 <- add.tips(T1, tips2add, where = where2add)
+    species_list$add[add_tip[ll > 0]] <- "genus"
+    add_tip <- add_tip[ll == 0]
 
-    if(length(add_tip)>0){
-      if("family" %in% colnames(species_list)){
+    if (length(add_tip) > 0) {
+      if ("family" %in% colnames(species_list)) {
         ind <- add_tip [species_list$family[add_tip] %in% T1$node.label]
-        if(length(ind)>0){
+        if (length(ind) > 0) {
           num <- match(species_list$family[ind], T1$node.label)
-                  + length(T1$tip.label)
-          T1 <- add.tips(T1, tips=species_list$species[ind], num)
+          +length(T1$tip.label)
+          T1 <- add.tips(T1, tips = species_list$species[ind], num)
           species_list$add[ind] <- "family"
         }
       }
@@ -121,9 +119,8 @@ phylobuilder <- function (species, tree)
 
   }
   toDrop <- setdiff(T1$tip.label,
-                    species_list$species[species_list$add!="missing"])
+    species_list$species[species_list$add != "missing"])
   T1 <- drop.tip(T1, tip = toDrop)
   attr(T1, "species_list") <- species_list
   T1
 }
-
