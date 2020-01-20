0e616c898b1defac21d6780bdee2845a03ff8ac5
Barnabas Daru <barnabasdaru@Barnabass-MBP.attlocal.net>
2020-01-20 02:10:03 -0600

    fix small bug

--- R/coldspots.R
+++ R/coldspots.R
@@ -70,8 +70,9 @@ coldspots <- function(x, prob = 2.5, ...) {
 hotspots <- function(x, prob = 2.5,...) {
   quant <- (1 - (prob / 100))
   r <- quantile(x, quant, na.rm = TRUE)
-  x[which(x < r[[1]])] <- 0
+  x[which(x < r[[1]])] <- NA
   x[which(x > r[[1]])] <- 1
   x[which(x == r[[1]])] <- 1
+  x[which(is.na(x))] <- 0
   x
 }
