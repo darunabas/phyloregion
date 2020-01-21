904665fbf2058218e5f4ea972e1d0cc8440f41f7
Klaus Schliep <klaus.schliep@gmail.com>
2020-01-21 21:57:34 +0100

    reexport some Matrix goodies

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,11 @@ Matrix::Matrix
 #' @importFrom Matrix sparseMatrix
 #' @export
 Matrix::sparseMatrix
+
+#' @importFrom Matrix colSums
+#' @export
+Matrix::colSums
+
+#' @importFrom Matrix rowSums
+#' @export
+Matrix::rowSums
