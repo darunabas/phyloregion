4086e623c40668797960a3af6303e15286e38372
Barnabas Daru <barnabasdaru@Barnabass-MacBook-Pro.local>
2020-11-17 15:30:58 -0600

    small edits to plot_structure

--- R/plot_structure.R
+++ R/plot_structure.R
@@ -15,9 +15,11 @@
 #' @rdname plot_structure
 #' @importFrom graphics Axis barplot layout
 #' @importFrom utils modifyList
+#' @importFrom sp coordinates<- CRS proj4string<- SpatialPolygonsDataFrame
+#' @importFrom sp coordinates<- over CRS proj4string merge
 #' @return Returns no value, just visualize structure plot!
 #' @export
-plot_structure <- function(omega, shp, by = NULL, col=hcl.colors(12), ...) {
+plot_structure <- function(omega, shp, by = NULL, col=hcl.colors(15), ...) {
 
     #shp <- obj$shp
     #omega <- obj$omega
@@ -26,10 +28,29 @@ plot_structure <- function(omega, shp, by = NULL, col=hcl.colors(12), ...) {
     s1 <- subset(shp, shp$grids %in% index)
 
     if (!is.null(by)) {
-        s1 <- s1[, c("grids", by)]
-        names(s1) <- c("grids", "by")
-        sx <- split(s1, f=s1$by)
-        vv <- as.numeric(matrix(lapply(sx, function(x) nrow(x)/nrow(s1))))
+        if (inherits(by, "SpatialPolygonsDataFrame")) {
+            w <- s1[, "grids"]
+            suppressWarnings(proj4string(by) <- proj4string(w))
+            r <- suppressWarnings(cbind(as.data.frame(w), sp::over(w, by)))
+            d <- merge(w, r, by="grids")
+            d <- d[, c(1, 2)]
+            names(d) <- c("grids", "by")
+            d <- d[!is.na(d@data$by),]
+        } else {
+            d <- s1[, c("grids", by)]
+            names(d) <- c("grids", "by")
+        }
+
+        tmp <- names(which(table(d$by)<2))
+        if(length(tmp)>0) {
+            print("Dropping sites because they have only one grid cell:")
+            print(tmp)
+        }
+
+        d <- subset(d, !(d$by %in% tmp))
+
+        sx <- split(d, f=d$by)
+        vv <- as.numeric(matrix(lapply(sx, function(x) nrow(x)/nrow(d))))
 
         nf <- layout(matrix(1:length(sx), ncol=1), heights=vv)
 
@@ -40,10 +61,9 @@ plot_structure <- function(omega, shp, by = NULL, col=hcl.colors(12), ...) {
             par(mgp=c(3,0,0))
             par(mar=c(0, 5, 0, 1))
             barplot(z1, horiz=TRUE, col=col,
-                    border=NA, space=0, las=1,
-                    axes=FALSE, ylab=x$by[1], cex.lab=0.75)
-
-        })))
+                    border = if (nrow(d) < 200) "black" else NA,
+                    space=0, las=1,
+                    axes=FALSE, ylab=x$by[1], cex.lab=0.75)})))
     } else {
         ind1 <- intersect(s1$grids, row.names(omega))
         y1 <- omega[ind1,]
