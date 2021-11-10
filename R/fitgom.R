df1a3deb8dde47267d2113ea6e4684b09ddc6c5d
Barnabas Daru <darunabas@gmail.com>
2021-11-10 14:22:36 -0600

    updated fitgom

--- R/fitgom.R
+++ R/fitgom.R
@@ -4,6 +4,23 @@
 #' or “Latent Dirichlet Allocation” models, by representing sampling units
 #' as partial memberships in multiple groups. It can group regions
 #' based on phylogenetic information or functional traits.
+#'
+#' Mapping phylogenetic regions (phyloregions) involves successively slicing
+#' the phylogenetic tree at various time depths (e.g., from 1, 2, 3, to 5
+#' million years ago (Ma)), collapsing nodes and ranges that originated at
+#' each time depth, and generating a new community matrix based on the presence
+#' or absence of each lineage in a grid cell. A grade of membership model is
+#' then fitted to the reduced community matrix. To map functional trait regions
+#' (traitregions), the function uses k-means to cluster species based on their
+#' functional traits, often for mixed-type data including categorical and
+#' numeric functional traits. The ranges for each species in each resulting
+#' cluster are collapsed to generate a new community matrix based on the
+#' presence or absence of cluster representative in a grid cell. A grade of
+#' membership model is then fitted to the new reduced community matrix. Mapping
+#' bioregions for taxonomic diversity is based on fitting a grade of
+#' membership model directly to the original community matrix that is often
+#' represented with species in the columns and sites as rows.
+#'
 #' @param x A community data in long format with one column representing
 #' sites labeled \dQuote{grids} and another column representing species
 #' labeled \dQuote{species}.
@@ -85,17 +102,31 @@ fitgom <- function (x, trait = NULL, cut = NULL, phy = NULL, bin = 10,
     subphy <- keep.tip(phy, intersect(phy$tip.label, x$species))
     submat <- subset(x, x$species %in% intersect(phy$tip.label, x$species))
     tx <- get_clades(subphy, cut = cut, k = bin)
-    z <- length(tx)
-    memb <- rep(seq_len(z), lengths(tx))
-    sp <- sparseMatrix(seq_along(memb), j = memb, dims=c(length(memb),z),
-                       dimnames=list(unlist(tx), as.character(seq_len(z))))
-    M <- long2sparse(submat)
-    mx <- M %*% sp
+
+    # SPECIES THAT CAN BE LUMPED
+    m <- do.call(rbind, lapply(tx, function(y) cbind(species=y,
+                                                    cluster=parent.frame()$i[],
+                                                    grouping="lumper",
+                                                    ntax=length(y))))
+    r <- data.frame(m[m[, "ntax"] > 1,])
+    # MASTER_DISTINCT -- SPECIES WHICH ARE DISTINCT AND CAN'T BE LUMPED
+    w <- submat[!submat$species %in% as.vector(r$species), ]
+    spp <- unique(as.character(r$cluster))
+
+    spo <- lapply(spp, function(y) {
+      v <- r[r$cluster==y, ]
+      v <- submat[submat$species %in% as.vector(v$species), ]
+      cbind(v["grids"], species=v$species[1])
+    })
+
+    z <- rbind(w, do.call(rbind, spo))
+    mx <- long2sparse(z)
   } else if (!is.null(trait)) {
     trait <- as.data.frame(trait)
     trait <- trait[!duplicated(trait[ , "species"]),]
     trait$species <- gsub(" ", "_", trait$species)
     index <- intersect(x$species, trait$species)
+    submat <- x[x$species %in% index, ]
     trait <- subset(trait, trait$species %in% index)
 
     row.names(trait) <- trait$species
@@ -106,26 +137,40 @@ fitgom <- function (x, trait = NULL, cut = NULL, phy = NULL, bin = 10,
 
     x2 <- Filter(is.numeric, trait)
     x2[] <- apply(x2, 2, as.numeric)
-    m <- data.frame(x1, x2)
+    mm <- data.frame(x1, x2)
 
     # apply k-prototypes
-    m <- kproto(m, bin = bin, na.rm = na.rm)
+    mm <- kproto(mm, k = bin, na.rm = na.rm)
+    memb <- mm$cluster
 
-    memb <- m$cluster
-    #    names(memb) <- labels(g1)
-    #    memb <- memb[!(memb==0)]
-    z <- length(unique(memb))
+    foox <- function(x) {
+      k <- max(x)
+      res <- vector("list", k)
+      for (i in seq_len(k)) {
+        res[[i]] <- names(x)[x==i]
+      }
+      res
+    }
+    d <- foox(memb)
+    m <- do.call(rbind, lapply(d, function(y) cbind(species=y,
+                                                     cluster=parent.frame()$i[],
+                                                     grouping="lumper",
+                                                     ntax=length(y))))
+    r <- data.frame(m[m[, "ntax"] > 1,])
+    # MASTER_DISTINCT -- SPECIES WHICH ARE DISTINCT AND CAN'T BE LUMPED
+    w <- submat[!submat$species %in% as.vector(r$species), ]
+    spp <- unique(as.character(r$cluster))
 
-    sp <- sparseMatrix(seq_along(memb), j = memb, dims=c(length(memb),z),
-                       dimnames=list(labels(memb), as.character(seq_len(z))))
-
-    submat <- subset(x, x$species %in% index)
-    M <- long2sparse(submat)
-    mx <- M %*% sp
+    spo <- lapply(spp, function(y) {
+      v <- r[r$cluster==y, ]
+      v <- submat[submat$species %in% as.vector(v$species), ]
+      cbind(v["grids"], species=v$species[1])
+    })
+    z <- rbind(w, do.call(rbind, spo))
+    mx <- long2sparse(z)
   } else {
     mx <- long2sparse(x)
   }
-
   res <- topics(mx, K, shape = shape, initopics = initopics, tol = tol,
                 bf = bf, kill = kill, ord = ord, verb = verb, ...)
   return(res)
